ccaad69716415bbda26f618a3f18f0ff681f80ec
Luis Gabriel Hernandez <lghernandez@gmail.com>
2021-02-24 00:23:33 -0500

    Format completed

--- generate_files.py
+++ generate_files.py
@@ -11,6 +11,45 @@ with open("D:\\DDI-Bidirectional.csv", "w", newline="") as final:
             original,
             delimiter="\t",
         )
+        writer = csv.writer(final, quoting=csv.QUOTE_ALL, delimiter=",")
+        writer.writerow(
+            [
+                "Source ID",
+                "Source",
+                "ISO Code",
+                "Country",
+                "Digits",
+                "Last Modification",
+                "Source Type",
+                "Mask",
+                "Mask Active",
+                "CPC",
+                "NumA",
+                "Group Country NumA",
+                "Business Unit",
+                "MNC",
+                "Rate",
+                "Billing Increments",
+            ]
+        )
         for rows in reader:
-            writer = csv.writer(final, quoting=csv.QUOTE_ALL, delimiter=",")
-            writer.writerow(rows)
+            writer.writerow(
+                [
+                    "MEX99",
+                    "DDI-Bidirectional",
+                    "52",
+                    "MEXICO",
+                    rows[0],
+                    "",
+                    "Fixed",
+                    rows[0],
+                    "1",
+                    "0",
+                    "",
+                    "",
+                    "",
+                    rows[1],
+                    "0,0",
+                    "1/1",
+                ]
+            )
